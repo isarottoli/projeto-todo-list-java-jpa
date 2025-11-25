00aef6dd6d0df693dbe7b3c29671b30b7a416531
Artur Schincariol Rossi <artursrossi4@gmail.com>
2025-11-25 09:15:02 -0300

    feat: new db table 'tb_tasks' + Route for list all tasks (GET /tasks/)

---
+++ src/main/java/todo_list/task/TaskController.java
@@ -0,0 +1,23 @@
+package todo_list.task;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private ITaskRepository taskRepository;
+
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request){
+        return taskRepository.findAll();
+    } 
+}
---
+++ src/main/java/todo_list/task/TaskModel.java
@@ -0,0 +1,48 @@
+package todo_list.task;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "tb_tasks")
+public class TaskModel {
+    
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    
+    @Column(nullable = false, length = 48)
+    private String title;
+    
+    @Column(length = 512)
+    private String description;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TaskStatus status = TaskStatus.TODO;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TaskPriority priority = TaskPriority.NORMAL;
+    
+    @Column(name = "start_at")
+    private LocalDateTime startAt;
+    
+    @Column(name = "end_at")
+    private LocalDateTime endAt;
+    
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
